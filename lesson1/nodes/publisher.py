3424440ad6a7536a7d1588778814dfcb84775ad9
Suyamprakasam Varadaraj <2400683@sit.singaporetech.edu.sg>
2026-07-29 09:59:47 +0300

    Removed comments (Code Quality)

--- lesson1/nodes/publisher.py
+++ lesson1/nodes/publisher.py
@@ -5,9 +5,9 @@ from std_msgs.msg import String
 
 class Publisher:
     def __init__(self):
-        self.rate = rospy.get_param('~rate', 2) # 2hz per second, 0.5s per cycle
+        self.rate = rospy.get_param('~rate', 2) 
         self.rate = rospy.Rate(self.rate)
-        self.message = rospy.get_param('~message', 'Hello World!') # Get the message parameter from the launch file
+        self.message = rospy.get_param('~message', 'Hello World!') 
 
         # Publishers
         self.pub = rospy.Publisher('/message', String, queue_size=10) # Create a publisher that will publish messages of type String to the topic '/message' with a queue size of 10
@@ -15,7 +15,7 @@ class Publisher:
     def run(self):
         while not rospy.is_shutdown():
             self.pub.publish(self.message) # Publish the message to the topic 
-            self.rate.sleep() # Pauses the loop for the time remaining to hit the rate (2hz)
+            self.rate.sleep() 
 
 if __name__ == '__main__':
     rospy.init_node('publisher') # Initialize the node with the name 'publisher'
