b6fee513292c841f50be4c4a77120900d4729615
Suyamprakasam Varadaraj <2400683@sit.singaporetech.edu.sg>
2026-07-29 10:04:11 +0300

    Resolve parameter and internal variables initialization

--- lesson1/nodes/publisher.py
+++ lesson1/nodes/publisher.py
@@ -5,9 +5,12 @@ from std_msgs.msg import String
 
 class Publisher:
     def __init__(self):
-        self.rate = rospy.get_param('~rate', 2) 
-        self.rate = rospy.Rate(self.rate)
+        # Parameters
         self.message = rospy.get_param('~message', 'Hello World!') 
+        rate = rospy.get_param('~rate', 2) 
+        
+        # Internal variables
+        self.rate = rospy.Rate(self.rate)
 
         # Publishers
         self.pub = rospy.Publisher('/message', String, queue_size=10) # Create a publisher that will publish messages of type String to the topic '/message' with a queue size of 10
